7a3a4fd12ca88066d5d50096651718bc16f3ed27
Aditya Atbhaiya <126330557+AdityaAtb09@users.noreply.github.com>
2023-12-17 16:02:42 +0530

    Time: 7 ms (85.77%), Space: 11.2 MB (51.71%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -3,12 +3,12 @@ public:
     string destCity(vector<vector<string>>& paths) {
         unordered_set<string> st;
         
-        for(auto it : paths)
+        for(const auto& it : paths)
         {
             st.insert(it[0]);
         }
         
-        for(auto it : paths)
+        for(const auto& it : paths)
         {
             if(st.find(it[1]) == st.end())
             {
