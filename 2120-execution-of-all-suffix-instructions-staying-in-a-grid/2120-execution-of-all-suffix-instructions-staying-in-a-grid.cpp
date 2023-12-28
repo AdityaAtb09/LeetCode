39b5a7f911cf94d6da914b980ec583e1053d9982
Aditya Atbhaiya <126330557+AdityaAtb09@users.noreply.github.com>
2023-12-28 20:21:04 +0530

    Time: 27 ms (64.34%), Space: 10.4 MB (99.18%) - LeetHub

---
+++ 2120-execution-of-all-suffix-instructions-staying-in-a-grid/2120-execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        vector<int> ans(s.length());
+        int idx = 0;
+        int row = startPos[0];
+        int col = startPos[1];
+
+        for(int i=0; i<s.length(); i++)
+        {
+            int cnt = 0;
+            
+            for(int j=i; j<s.length(); j++)
+            {
+                if(s[j] == 'L') 
+                {
+                    col -= 1;
+                } 
+                else if(s[j] == 'R')
+                {
+                    col += 1;
+                } 
+                else if(s[j] == 'U') 
+                {
+                    row -= 1;
+                }
+                else
+                {
+                    row += 1;
+                }
+
+                if(row>=0 && row<n && col>=0 && col<n)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            row = startPos[0];
+            col = startPos[1];
+            ans[idx++] = cnt;
+        }
+        
+        return ans;
+    }
+};
