4cc75e53e1123cd6d847062a345d50e194a3773a
Aditya Atbhaiya <126330557+AdityaAtb09@users.noreply.github.com>
2023-12-29 17:25:30 +0530

    Time: 23 ms (10.81%), Space: 9.3 MB (91.50%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<string> ans;
+        int n = words[0].length();
+
+        for(int i = 0; i < n; i++)
+        {
+            char ch = words[0][i];
+            int j; 
+
+            for(j = 1; j < words.size(); j++)
+            {
+                int pos = words[j].find(ch);
+                
+                if(pos == string::npos)
+                {
+                    break;
+                }
+                words[j].erase(pos, 1);
+            }
+
+            if(j == words.size())
+            {
+                ans.push_back(string(1, ch));
+            }
+        }
+        
+        return ans;
+    }
+};
